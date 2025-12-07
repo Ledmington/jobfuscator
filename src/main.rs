5cc1b758816e230e6f61269333306f859f99e442
Ledmington <ledmington.dev@gmail.com>
2025-12-07 16:08:18 +0100

    Improved

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,159 @@
 use std::env;
 use std::fs::File;
 use std::io::{self, BufReader, Read};
+use std::path::{Path, PathBuf};
+
+pub enum Endian {
+    Little,
+    Big,
+}
+
+pub struct BinaryReader<'a> {
+    buf: &'a [u8],
+    pos: usize,
+    endian: Endian,
+}
+
+impl<'a> BinaryReader<'a> {
+    pub fn new(buf: &'a [u8], endian: Endian) -> Self {
+        Self {
+            buf,
+            pos: 0,
+            endian,
+        }
+    }
+
+    fn read_bytes(&mut self, count: usize) -> io::Result<&'a [u8]> {
+        assert!(count > 0);
+        if self.pos + count > self.buf.len() {
+            return Err(io::Error::new(
+                io::ErrorKind::UnexpectedEof,
+                "Not enough bytes",
+            ));
+        }
+        let slice = &self.buf[self.pos..self.pos + count];
+        self.pos += count;
+        Ok(slice)
+    }
+
+    pub fn read_u8(&mut self) -> io::Result<u8> {
+        Ok(self.read_bytes(1)?[0])
+    }
+
+    pub fn read_u16(&mut self) -> io::Result<u16> {
+        let bytes: [u8; 2] = self.read_bytes(2).unwrap().try_into().unwrap();
+        Ok(match self.endian {
+            Endian::Little => u16::from_le_bytes(bytes),
+            Endian::Big => u16::from_be_bytes(bytes),
+        })
+    }
+
+    pub fn read_u16_vec(&mut self, count: usize) -> io::Result<Vec<u16>> {
+        assert!(count > 0);
+        let mut res: Vec<u16> = vec![0u16; count];
+        for i in 0..count {
+            res[i] = self.read_u16().unwrap();
+        }
+        Ok(res)
+    }
+
+    pub fn read_u32(&mut self) -> io::Result<u32> {
+        let bytes: [u8; 4] = self.read_bytes(4).unwrap().try_into().unwrap();
+        Ok(match self.endian {
+            Endian::Little => u32::from_le_bytes(bytes),
+            Endian::Big => u32::from_be_bytes(bytes),
+        })
+    }
+}
+
+/**
+ * Specification available at https://docs.oracle.com/javase/specs/jvms/se25/html/jvms-4.html
+ */
+struct ClassFile {
+    absolute_file_path: String,
+    minor_version: u16,
+    major_version: u16,
+    constant_pool: Vec<ConstantPoolEntry>,
+    access_flags: u16,
+    this_class: u16,
+    super_class: u16,
+    interfaces: Vec<u16>,
+}
+
+struct ConstantPoolEntry {
+    tag: u8,
+    info: Vec<u8>,
+}
+
+fn absolute_no_symlinks(p: &Path) -> std::io::Result<PathBuf> {
+    if p.is_absolute() {
+        Ok(p.to_path_buf())
+    } else {
+        Ok(env::current_dir()?.join(p))
+    }
+}
+
+fn parse_class_file(filename: String) -> ClassFile {
+    let abs_file_path = absolute_no_symlinks(Path::new(&filename)).unwrap();
+    let file = File::open(&abs_file_path).expect("File does not exist");
+    let mut file_reader = BufReader::new(file);
+    let mut file_bytes: Vec<u8> = vec![0u8; file_reader.capacity()];
+    file_reader
+        .read_to_end(&mut file_bytes)
+        .expect("Could not read whole file");
+
+    let mut reader = BinaryReader::new(&file_bytes, Endian::Big);
+
+    let actual_magic_number: u32 = reader.read_u32().unwrap();
+    const EXPECTED_MAGIC_NUMBER: u32 = 0xcafebabe;
+    if actual_magic_number != EXPECTED_MAGIC_NUMBER {
+        eprintln!(
+            "Wrong magic number: expected 0x{:08x} but was 0x{:08x}.",
+            EXPECTED_MAGIC_NUMBER, actual_magic_number
+        );
+    }
+
+    let minor_version: u16 = reader.read_u16().unwrap();
+    let major_version: u16 = reader.read_u16().unwrap();
+
+    let cp_count: u16 = reader.read_u16().unwrap();
+    let cp: Vec<ConstantPoolEntry> = Vec::with_capacity((cp_count - 1).into());
+
+    let flags: u16 = reader.read_u16().unwrap();
+
+    let this_class: u16 = reader.read_u16().unwrap();
+    let super_class: u16 = reader.read_u16().unwrap();
+
+    let interfaces_count: u16 = reader.read_u16().unwrap();
+    let interfaces: Vec<u16> = reader.read_u16_vec(interfaces_count.into()).unwrap();
+
+    return ClassFile {
+        absolute_file_path: abs_file_path.to_str().unwrap().to_string(),
+        minor_version: minor_version,
+        major_version: major_version,
+        constant_pool: cp,
+        access_flags: flags,
+        this_class: this_class,
+        super_class: super_class,
+        interfaces: interfaces,
+    };
+}
+
+fn print_class_file(classfile: &ClassFile) -> () {
+    println!("Classfile {}", classfile.absolute_file_path);
+    println!("  minor version: {}", classfile.minor_version);
+    println!("  major version: {}", classfile.major_version);
+    println!("  flags: {:04x}", classfile.access_flags);
+    println!("  this class: {}", classfile.this_class);
+    println!("  super class: {}", classfile.super_class);
+}
 
 fn main() -> io::Result<()> {
     let filename = env::args().nth(1).expect("Usage: program <filename>");
 
-    let file = File::open(&filename)?;
-    let mut reader = BufReader::new(file);
-    let mut file_bytes: Vec<u8> = vec![0u8; reader.capacity()];
-    reader.read_to_end(&mut file_bytes)?;
+    let classfile: ClassFile = parse_class_file(filename);
 
-    for i in 0..file_bytes.len() {
-        let b = file_bytes[i];
-        print!(" {:#04x}", b);
-        if i % 16 == 15 {
-            println!();
-        }
-    }
-    println!();
+    print_class_file(&classfile);
 
     Ok(())
 }
