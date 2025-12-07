bedc398832edd740a56f99b4b4eb1affba24b393
Ledmington <ledmington.dev@gmail.com>
2025-12-07 15:08:16 +0100

    Added read from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs::File;
+use std::io::{self, BufReader, Read};
+
+fn main() -> io::Result<()> {
+    let filename = env::args().nth(1).expect("Usage: program <filename>");
+
+    let file = File::open(&filename)?;
+    let mut reader = BufReader::new(file);
+    let mut file_bytes: Vec<u8> = vec![0u8; reader.capacity()];
+    reader.read_to_end(&mut file_bytes)?;
+
+    for i in 0..file_bytes.len() {
+        let b = file_bytes[i];
+        print!(" {:#04x}", b);
+        if i % 16 == 15 {
+            println!();
+        }
+    }
+    println!();
+
+    Ok(())
 }
