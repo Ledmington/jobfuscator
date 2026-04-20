218c997672a587a4465d43e151a8ab10aef8d210
Ledmington <ledmington.dev@gmail.com>
2026-04-20 12:53:07 +0200

    Refactoring

--- cli_parser/src/lib.rs
+++ cli_parser/src/lib.rs
@@ -1,6 +1,6 @@
 #![forbid(unsafe_code)]
 
-use std::{env::Args, iter::Map};
+use std::{collections::HashMap, env::Args, fmt};
 
 pub struct CommandLineParser {
     program_name: String,
@@ -8,22 +8,47 @@ pub struct CommandLineParser {
     options: Vec<CommandLineOption>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct CommandLineOption {
     short_name: Option<String>,
     long_name: Option<String>,
     option_type: CommandLineType,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub enum CommandLineType {
     Boolean { default_value: Option<bool> },
     String { default_value: Option<String> },
 }
 
+impl fmt::Display for CommandLineType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            CommandLineType::Boolean { default_value } => match default_value {
+                Some(v) => write!(f, "bool (default: {v})"),
+                None => write!(f, "bool"),
+            },
+            CommandLineType::String { default_value } => match default_value {
+                Some(v) => write!(f, "string (default: \"{v}\")"),
+                None => write!(f, "string"),
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub enum CommandLineValue {
-    Boolean { value: bool },
-    String { value: String },
+    Boolean(bool),
+    String(String),
+}
+
+impl CommandLineValue {
+    pub fn as_bool(&self) -> bool {
+        match self {
+            CommandLineValue::Boolean(value) => *value,
+            _ => panic!("This value is not a boolean."),
+        }
+    }
 }
 
 impl CommandLineOption {
@@ -48,12 +73,41 @@ impl CommandLineOption {
         }
     }
 
-    fn matches(&self, arg: &String) -> Result<CommandLineValue, String> {
-        let value: CommandLineValue = match &self.option_type {
-            CommandLineType::Boolean { default_value } => todo!(),
-            CommandLineType::String { default_value } => todo!(),
-        };
-        Ok(value)
+    fn canonical_name(&self) -> &str {
+        self.long_name
+            .as_deref()
+            .or(self.short_name.as_deref())
+            .unwrap()
+    }
+
+    fn usage_names(&self) -> String {
+        match (&self.short_name, &self.long_name) {
+            (Some(s), Some(l)) => format!("{s}, {l}"),
+            (Some(s), None) => s.clone(),
+            (None, Some(l)) => l.clone(),
+            (None, None) => unreachable!(),
+        }
+    }
+
+    fn try_match(&self, argument_name: &str, argument_value: &str) -> Option<CommandLineValue> {
+        let matches_short_name: bool =
+            self.short_name.is_some() && self.short_name.clone().unwrap() == argument_name;
+        let matches_long_name: bool =
+            self.long_name.is_some() && self.long_name.clone().unwrap() == argument_name;
+        if !matches_short_name && !matches_long_name {
+            return None;
+        }
+
+        match self.option_type {
+            CommandLineType::Boolean { .. } => match argument_value {
+                "0" | "false" => Some(CommandLineValue::Boolean(false)),
+                "1" | "true" => Some(CommandLineValue::Boolean(true)),
+                _ => panic!("'{}' is not a valid boolean value.", argument_value),
+            },
+            CommandLineType::String { .. } => {
+                Some(CommandLineValue::String(argument_value.to_owned()))
+            }
+        }
     }
 }
 
@@ -63,9 +117,8 @@ impl CommandLineParser {
         description: Option<String>,
         options: Vec<CommandLineOption>,
     ) -> Self {
-        let mut actual_options: Vec<CommandLineOption> = options.clone();
+        let mut actual_options = options;
 
-        // Adding the default '-h'/'--help' option
         actual_options.push(CommandLineOption {
             short_name: Some("-h".to_string()),
             long_name: Some("--help".to_string()),
@@ -95,41 +148,152 @@ impl CommandLineParser {
         }
     }
 
-    pub fn parse(&self, args: &Args) -> Arguments {
-        let mut args_str: Vec<String> = Vec::with_capacity(args.len());
-        for arg in args.skip(0) {
-            args_str.push(arg);
+    fn print_help(&self) {
+        println!("Usage: {}", self.program_name);
+
+        if let Some(desc) = &self.description {
+            println!("{desc}");
         }
+
+        println!("\nOptions:");
+
+        for option in &self.options {
+            println!("  {:<24} {}", option.usage_names(), option.option_type);
+        }
+    }
+
+    pub fn parse(&self, args: Args) -> Arguments {
+        let args_str: Vec<String> = args.skip(1).collect();
         self.parse_str(&args_str)
     }
 
     pub fn parse_str(&self, args: &[String]) -> Arguments {
-        for arg in args {
-            for option in self.options {
-                let result = option.matches(arg);
-                if result.is_ok() {}
+        let mut values: HashMap<String, CommandLineValue> = HashMap::new();
+
+        // Load defaults
+        for option in &self.options {
+            let key = option.canonical_name().to_string();
+            match &option.option_type {
+                CommandLineType::Boolean {
+                    default_value: Some(v),
+                } => {
+                    values.insert(key, CommandLineValue::Boolean(*v));
+                }
+                CommandLineType::String {
+                    default_value: Some(v),
+                } => {
+                    values.insert(key, CommandLineValue::String(v.clone()));
+                }
+                _ => {}
             }
         }
-        Arguments {}
+
+        let mut i = 0;
+        while i < args.len() {
+            let arg = &args[i];
+
+            let argument_name: &str;
+            let argument_value: &str;
+
+            if arg.starts_with('-') {
+                let contains_equals: bool = arg.contains('=');
+                if contains_equals {
+                    let equals_pos = arg.find('=').unwrap();
+                    argument_name = &arg[1..equals_pos];
+                    argument_value = &arg[(equals_pos + 1)..];
+                } else {
+                    if i + 1 >= args.len() {
+                        panic!(
+                            "Option '{}' expected an argument but found end of input.",
+                            arg
+                        );
+                    }
+                    argument_name = &arg[1..];
+                    argument_value = &args[i + 1];
+                    i += 1;
+                }
+            } else if arg.starts_with("--") {
+                let contains_equals: bool = arg.contains('=');
+                if contains_equals {
+                    let equals_pos = arg.find('=').unwrap();
+                    argument_name = &arg[2..equals_pos];
+                    argument_value = &arg[(equals_pos + 1)..];
+                } else {
+                    if i + 1 >= args.len() {
+                        panic!(
+                            "Option '{}' expected an argument but found end of input.",
+                            arg
+                        );
+                    }
+                    argument_name = &arg[2..];
+                    argument_value = &args[i + 1];
+                    i += 1;
+                }
+            } else {
+                panic!("Expected an argument but found '{}'.", arg);
+            }
+
+            let mut matched = false;
+            for option in &self.options {
+                if let Some(parsed_value) = option.try_match(argument_name, argument_value) {
+                    if let Some(sn) = &option.short_name {
+                        values.insert(sn.to_string(), parsed_value.clone());
+                    }
+                    if let Some(ln) = &option.long_name {
+                        values.insert(ln.to_string(), parsed_value.clone());
+                    }
+                    matched = true;
+                    break;
+                }
+            }
+
+            if !matched {
+                eprintln!("Warning: unrecognized argument '{argument_name}'");
+            }
+
+            i += 1;
+        }
+
+        if matches!(values.get("--help"), Some(CommandLineValue::Boolean(true))) {
+            self.print_help();
+            std::process::exit(0);
+        }
+
+        Arguments { values }
     }
 }
 
 pub struct Arguments {
-    values: Map<String, CommandLineValue>,
+    values: HashMap<String, CommandLineValue>,
 }
 
 impl Arguments {
-    pub fn get(&self, name: &str) -> Argument {
-        todo!()
+    pub fn get(&self, name: &str) -> Option<&CommandLineValue> {
+        self.values.get(name)
+    }
+
+    pub fn get_bool(&self, name: &str) -> Option<bool> {
+        match self.values.get(name) {
+            Some(CommandLineValue::Boolean(b)) => Some(*b),
+            _ => None,
+        }
+    }
+
+    pub fn get_string(&self, name: &str) -> Option<&str> {
+        match self.values.get(name) {
+            Some(CommandLineValue::String(s)) => Some(s.as_str()),
+            _ => None,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
     #[test]
-    fn parse_boolean_option() {
+    fn parse_default_boolean_option() {
         let parser = CommandLineParser::new(
             "test-parser",
             Some("A parser for testing".to_string()),
@@ -142,19 +306,64 @@ mod tests {
             }],
         );
 
-        for input in [
-            "-q",
-            "-q 1",
-            "-q true",
-            "-q=1",
-            "-q=true",
-            "--quiet",
-            "--quiet 1",
-            "--quiet true",
-            "--quiet=1",
-            "--quiet=true",
-        ] {
-            let args = parser.parse_str(&[input.to_owned()]);
-        }
+        let args = parser.parse_str(&vec![]);
+        assert_eq!(false, args.get("q").unwrap().as_bool());
+        assert_eq!(false, args.get("quiet").unwrap().as_bool());
+    }
+
+    #[rstest]
+    #[case("-q")]
+    #[case("-q 1")]
+    #[case("-q true")]
+    #[case("-q=1")]
+    #[case("-q=true")]
+    #[case("--quiet")]
+    #[case("--quiet 1")]
+    #[case("--quiet true")]
+    #[case("--quiet=1")]
+    #[case("--quiet=true")]
+    fn parse_boolean_option_true(#[case] input: &str) {
+        let parser = CommandLineParser::new(
+            "test-parser",
+            Some("A parser for testing".to_string()),
+            vec![CommandLineOption {
+                short_name: Some("-q".to_string()),
+                long_name: Some("--quiet".to_string()),
+                option_type: CommandLineType::Boolean {
+                    default_value: Some(false),
+                },
+            }],
+        );
+
+        let args = parser.parse_str(&[input.to_owned()]);
+        assert_eq!(true, args.get("q").unwrap().as_bool());
+        assert_eq!(true, args.get("quiet").unwrap().as_bool());
+    }
+
+    #[rstest]
+    #[case("-q 0")]
+    #[case("-q false")]
+    #[case("-q=0")]
+    #[case("-q=false")]
+    #[case("--quiet 0")]
+    #[case("--quiet false")]
+    #[case("--quiet=0")]
+    #[case("--quiet=false")]
+    fn parse_boolean_option_false(#[case] input: &str) {
+        let parser = CommandLineParser::new(
+            "test-parser",
+            Some("A parser for testing".to_string()),
+            vec![CommandLineOption {
+                short_name: Some("-q".to_string()),
+                long_name: Some("--quiet".to_string()),
+                option_type: CommandLineType::Boolean {
+                    default_value: Some(false),
+                },
+            }],
+        );
+
+        let args = parser.parse_str(&[input.to_owned()]);
+        assert_eq!(false, args.get("q").unwrap().as_bool());
+        assert_eq!(false, args.get("quiet").unwrap().as_bool());
     }
 }
