f603937837ad9a024e37b3641e1843f6621e72d1
Ledmington <ledmington.dev@gmail.com>
2026-04-26 14:12:51 +0200

    Fixed javap

--- javap/src/javap.rs
+++ javap/src/javap.rs
@@ -1369,11 +1369,11 @@ fn print_method_attributes(
                 }
                 if !exception_table.is_empty() {
                     lw.println("Exception table:");
-                    lw.println("   from    to  target type");
                     lw.indent(1);
+                    lw.println(" from    to  target type");
                     for exception in exception_table.iter() {
                         lw.println(&format!(
-                            "    {}  {}  {}   Class {}",
+                            " {:5} {:5} {:5}   Class {}",
                             exception.start_pc,
                             exception.end_pc,
                             exception.handler_pc,
@@ -1748,6 +1748,25 @@ fn print_class_attributes(lw: &mut LineWriter, cp: &ConstantPool, attributes: &[
                 for class_index in classes {
                     lw.println(&format!("  {}", cp.get_class_name(*class_index)));
                 }
+            }
+            AttributeInfo::EnclosingMethod {
+                class_index,
+                method_index,
+                ..
+            } => {
+                lw.print("EnclosingMethod: ")
+                    .print(&format!("#{}.#{}", class_index, method_index))
+                    .tab()
+                    .print("// ")
+                    .println(&cp.get_class_name(*class_index).replace('/', "."));
+            }
+            AttributeInfo::NestHost {
+                host_class_index, ..
+            } => {
+                lw.println(&format!(
+                    "NestHost: class {}",
+                    cp.get_class_name(*host_class_index)
+                ));
             }
             _ => unreachable!(),
         }
