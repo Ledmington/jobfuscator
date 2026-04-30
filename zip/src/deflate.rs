6ed010e11c2b8cef6ecb8fbbd30f59c522c4b67a
Ledmington <ledmington.dev@gmail.com>
2026-04-30 18:26:46 +0200

    wip

--- zip/src/deflate.rs
+++ zip/src/deflate.rs
@@ -1,6 +1,10 @@
 use binary_reader::bit_reader::BitReader;
 use binary_writer::bit_writer::BitWriter;
 
+/*
+ * Reference: https://www.rfc-editor.org/rfc/rfc1951.txt.
+ */
+
 pub fn decompress(input: &Vec<u8>) -> Vec<u8> {
     let mut reader = BitReader::new(input);
     let mut writer = BitWriter::new();
@@ -19,6 +23,18 @@ pub fn decompress(input: &Vec<u8>) -> Vec<u8> {
         match btype {
             0b00 => {
                 // The block is uncompressed
+                let len: u16 = reader.read_u16().unwrap();
+                {
+                    let nlen: u16 = reader.read_u16().unwrap();
+                    assert!(
+                        !len == nlen,
+                        "Expected NLEN (0x{:04x}) to be one's complement of LEN (0x{:04x}) but it wasn't.",
+                        len,
+                        nlen
+                    );
+                }
+
+                // follows LEN bytes of literal data...
             }
             0b01 => {
                 // Static Huffman
