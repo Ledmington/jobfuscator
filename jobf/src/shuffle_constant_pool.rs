871fd07b14dfb33f82e4618edc66d5a8e9eddf5b
Ledmington <ledmington.dev@gmail.com>
2026-05-02 18:47:23 +0200

    Minor fixes

--- jobf/src/shuffle_constant_pool.rs
+++ jobf/src/shuffle_constant_pool.rs
@@ -20,50 +20,41 @@ impl ShuffleConstantPool {
         ShuffleConstantPool { seed }
     }
 
-    fn shuffle_indices(&self, cp: &ConstantPool) -> HashMap<u16, u16> {
+    fn shuffle_indices(&self, cp: &ConstantPool) -> CPIndexMap {
         // We cannot just shuffle all the indices, we need to shuffle just the indices of all entries that are not NULL,
         // Then re-insert them after LONG and DOUBLE entries.
 
-        let mut new_cp_indices: Vec<u16> = (0..cp.entries.len())
-            .into_iter()
+        let non_null_indices: Vec<u16> = (0..cp.entries.len())
             .filter(|idx| !matches!(cp[(*idx).try_into().unwrap()], ConstantPoolInfo::Null {}))
-            .map(|idx| idx.try_into().unwrap())
+            .map(|idx| idx as u16)
             .collect();
 
+        let mut shuffled = non_null_indices.clone();
         let mut rng = ChaCha8Rng::seed_from_u64(self.seed);
-        new_cp_indices.shuffle(&mut rng);
+        shuffled.shuffle(&mut rng);
 
+        // Map old index -> new index
         let mut cp_index_map: HashMap<u16, u16> = HashMap::new();
-        for i in 0..cp.entries.len() {
-            cp_index_map.insert(i.try_into().unwrap(), new_cp_indices[i].try_into().unwrap());
+        for (new_pos, &old_idx) in shuffled.iter().enumerate() {
+            cp_index_map.insert(old_idx, new_pos as u16);
         }
-        assert!(cp_index_map.len() == new_cp_indices.len());
 
-        {
-            let mut index: u16 = 0;
-            while index < new_cp_indices.len().try_into().unwrap() {
-                if matches!(cp[index], ConstantPoolInfo::Long { .. })
-                    || matches!(cp[index], ConstantPoolInfo::Double { .. })
-                {
-                    cp_index_map.insert(cp_index_map.get(&index).unwrap() + 1, index + 1);
-
-                    index += 2;
-                } else {
-                    index += 1;
-                }
+        // Fix up the implicit Null slots after Long/Double
+        // Wherever old_idx is a Long/Double, old_idx+1 must follow new_idx+1
+        for &old_idx in &shuffled {
+            if matches!(
+                cp[old_idx],
+                ConstantPoolInfo::Long { .. } | ConstantPoolInfo::Double { .. }
+            ) {
+                let new_idx = *cp_index_map.get(&old_idx).unwrap();
+                cp_index_map.insert(old_idx + 1, new_idx + 1);
             }
         }
 
-        assert!(cp_index_map.len() == cp.len());
-
-        return cp_index_map;
+        CPIndexMap { map: cp_index_map }
     }
 
-    fn modify_constant_pool(
-        &self,
-        cp_index_map: &HashMap<u16, u16>,
-        cp: &ConstantPool,
-    ) -> ConstantPool {
+    fn modify_constant_pool(&self, cp_index_map: &CPIndexMap, cp: &ConstantPool) -> ConstantPool {
         let mut new_cp_entries: Vec<ConstantPoolInfo> = Vec::with_capacity(cp.len());
         for entry in cp.entries.iter() {
             new_cp_entries.push(match entry {
@@ -74,57 +65,55 @@ impl ShuffleConstantPool {
                 | ConstantPoolInfo::Double { .. }
                 | ConstantPoolInfo::Null {} => entry.clone(),
                 ConstantPoolInfo::String { string_index } => ConstantPoolInfo::String {
-                    string_index: *cp_index_map.get(string_index).unwrap(),
+                    string_index: cp_index_map.get(*string_index),
                 },
                 ConstantPoolInfo::Class { name_index } => ConstantPoolInfo::Class {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
                 },
                 ConstantPoolInfo::FieldRef {
                     class_index,
                     name_and_type_index,
                 } => ConstantPoolInfo::FieldRef {
-                    class_index: *cp_index_map.get(class_index).unwrap(),
-                    name_and_type_index: *cp_index_map.get(name_and_type_index).unwrap(),
+                    class_index: cp_index_map.get(*class_index),
+                    name_and_type_index: cp_index_map.get(*name_and_type_index),
                 },
                 ConstantPoolInfo::MethodRef {
                     class_index,
                     name_and_type_index,
                 } => ConstantPoolInfo::MethodRef {
-                    class_index: *cp_index_map.get(class_index).unwrap(),
-                    name_and_type_index: *cp_index_map.get(name_and_type_index).unwrap(),
+                    class_index: cp_index_map.get(*class_index),
+                    name_and_type_index: cp_index_map.get(*name_and_type_index),
                 },
                 ConstantPoolInfo::InterfaceMethodRef {
                     class_index,
                     name_and_type_index,
                 } => ConstantPoolInfo::InterfaceMethodRef {
-                    class_index: *cp_index_map.get(class_index).unwrap(),
-                    name_and_type_index: *cp_index_map.get(name_and_type_index).unwrap(),
+                    class_index: cp_index_map.get(*class_index),
+                    name_and_type_index: cp_index_map.get(*name_and_type_index),
                 },
                 ConstantPoolInfo::NameAndType {
                     name_index,
                     descriptor_index,
                 } => ConstantPoolInfo::NameAndType {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
-                    descriptor_index: *cp_index_map.get(descriptor_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
+                    descriptor_index: cp_index_map.get(*descriptor_index),
                 },
                 ConstantPoolInfo::MethodType { descriptor_index } => ConstantPoolInfo::MethodType {
-                    descriptor_index: *cp_index_map.get(descriptor_index).unwrap(),
+                    descriptor_index: cp_index_map.get(*descriptor_index),
                 },
                 ConstantPoolInfo::MethodHandle {
                     reference_kind,
                     reference_index,
                 } => ConstantPoolInfo::MethodHandle {
                     reference_kind: *reference_kind,
-                    reference_index: *cp_index_map.get(reference_index).unwrap(),
+                    reference_index: cp_index_map.get(*reference_index),
                 },
                 ConstantPoolInfo::InvokeDynamic {
                     bootstrap_method_attr_index,
                     name_and_type_index,
                 } => ConstantPoolInfo::InvokeDynamic {
-                    bootstrap_method_attr_index: *cp_index_map
-                        .get(bootstrap_method_attr_index)
-                        .unwrap(),
-                    name_and_type_index: *cp_index_map.get(name_and_type_index).unwrap(),
+                    bootstrap_method_attr_index: cp_index_map.get(*bootstrap_method_attr_index),
+                    name_and_type_index: cp_index_map.get(*name_and_type_index),
                 },
             });
         }
@@ -135,7 +124,7 @@ impl ShuffleConstantPool {
 
     fn modify_fields(
         &self,
-        cp_index_map: &HashMap<u16, u16>,
+        cp_index_map: &CPIndexMap,
         cp: &ConstantPool,
         fields: &Vec<FieldInfo>,
     ) -> Vec<FieldInfo> {
@@ -143,8 +132,8 @@ impl ShuffleConstantPool {
         for field in fields {
             new_fields.push(FieldInfo {
                 access_flags: field.access_flags,
-                name_index: *cp_index_map.get(&field.name_index).unwrap(),
-                descriptor_index: *cp_index_map.get(&field.descriptor_index).unwrap(),
+                name_index: cp_index_map.get(field.name_index),
+                descriptor_index: cp_index_map.get(field.descriptor_index),
                 attributes: self.modify_attributes(cp_index_map, cp, &field.attributes),
             });
         }
@@ -153,7 +142,7 @@ impl ShuffleConstantPool {
 
     fn modify_methods(
         &self,
-        cp_index_map: &HashMap<u16, u16>,
+        cp_index_map: &CPIndexMap,
         cp: &ConstantPool,
         methods: &Vec<MethodInfo>,
     ) -> Vec<MethodInfo> {
@@ -161,8 +150,8 @@ impl ShuffleConstantPool {
         for method in methods {
             new_methods.push(MethodInfo {
                 access_flags: method.access_flags,
-                name_index: *cp_index_map.get(&method.name_index).unwrap(),
-                descriptor_index: *cp_index_map.get(&method.descriptor_index).unwrap(),
+                name_index: cp_index_map.get(method.name_index),
+                descriptor_index: cp_index_map.get(method.descriptor_index),
                 attributes: self.modify_attributes(cp_index_map, cp, &method.attributes),
             });
         }
@@ -171,7 +160,7 @@ impl ShuffleConstantPool {
 
     fn modify_attributes(
         &self,
-        cp_index_map: &HashMap<u16, u16>,
+        cp_index_map: &CPIndexMap,
         cp: &ConstantPool,
         attributes: &Vec<AttributeInfo>,
     ) -> Vec<AttributeInfo> {
@@ -186,7 +175,7 @@ impl ShuffleConstantPool {
                     exception_table,
                     attributes,
                 } => AttributeInfo::Code {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
                     max_stack: *max_stack,
                     max_locals: *max_locals,
                     code: code.clone(),
@@ -197,94 +186,121 @@ impl ShuffleConstantPool {
                     name_index,
                     line_number_table,
                 } => AttributeInfo::LineNumberTable {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
                     line_number_table: line_number_table.clone(),
                 },
                 AttributeInfo::LocalVariableTable {
                     name_index,
                     local_variable_table,
                 } => AttributeInfo::LocalVariableTable {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
                     local_variable_table: local_variable_table.clone(),
                 },
                 AttributeInfo::LocalVariableTypeTable {
                     name_index,
                     local_variable_type_table,
-                } => todo!(),
+                } => AttributeInfo::LocalVariableTypeTable {
+                    name_index: cp_index_map.get(*name_index),
+                    local_variable_type_table: local_variable_type_table.clone(),
+                },
                 AttributeInfo::StackMapTable {
                     name_index,
                     stack_map_table,
                 } => AttributeInfo::StackMapTable {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
                     stack_map_table: stack_map_table.clone(),
                 },
                 AttributeInfo::SourceFile {
                     name_index,
                     source_file_index,
                 } => AttributeInfo::SourceFile {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
-                    source_file_index: *cp_index_map.get(source_file_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
+                    source_file_index: cp_index_map.get(*source_file_index),
                 },
                 AttributeInfo::BootstrapMethods {
                     name_index,
                     methods,
                 } => AttributeInfo::BootstrapMethods {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
                     methods: methods.clone(),
                 },
                 AttributeInfo::InnerClasses {
                     name_index,
                     classes,
                 } => AttributeInfo::InnerClasses {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
                     classes: classes.clone(),
                 },
                 AttributeInfo::MethodParameters {
                     name_index,
                     parameters,
-                } => todo!(),
+                } => AttributeInfo::MethodParameters {
+                    name_index: cp_index_map.get(*name_index),
+                    parameters: parameters.clone(),
+                },
                 AttributeInfo::Record {
                     name_index,
                     components,
-                } => todo!(),
+                } => AttributeInfo::Record {
+                    name_index: cp_index_map.get(*name_index),
+                    components: components.clone(),
+                },
                 AttributeInfo::Signature {
                     name_index,
                     signature_index,
-                } => todo!(),
+                } => AttributeInfo::Signature {
+                    name_index: cp_index_map.get(*name_index),
+                    signature_index: cp_index_map.get(*signature_index),
+                },
                 AttributeInfo::NestMembers {
                     name_index,
                     classes,
-                } => todo!(),
+                } => AttributeInfo::NestMembers {
+                    name_index: cp_index_map.get(*name_index),
+                    classes: classes
+                        .iter()
+                        .map(|class_idx| cp_index_map.get(*class_idx))
+                        .collect(),
+                },
                 AttributeInfo::RuntimeVisibleAnnotations {
                     name_index,
                     annotations,
-                } => todo!(),
+                } => AttributeInfo::RuntimeVisibleAnnotations {
+                    name_index: cp_index_map.get(*name_index),
+                    annotations: annotations.clone(),
+                },
                 AttributeInfo::ConstantValue {
                     name_index,
                     constant_value_index,
                 } => AttributeInfo::ConstantValue {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
-                    constant_value_index: *cp_index_map.get(constant_value_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
+                    constant_value_index: cp_index_map.get(*constant_value_index),
                 },
                 AttributeInfo::Exceptions {
                     name_index,
                     exception_indices,
-                } => todo!(),
+                } => AttributeInfo::Exceptions {
+                    name_index: cp_index_map.get(*name_index),
+                    exception_indices: exception_indices
+                        .iter()
+                        .map(|exception_index| cp_index_map.get(*exception_index))
+                        .collect(),
+                },
                 AttributeInfo::EnclosingMethod {
                     name_index,
                     class_index,
                     method_index,
                 } => AttributeInfo::EnclosingMethod {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
-                    class_index: *cp_index_map.get(class_index).unwrap(),
-                    method_index: *cp_index_map.get(method_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
+                    class_index: cp_index_map.get(*class_index),
+                    method_index: cp_index_map.get(*method_index),
                 },
                 AttributeInfo::NestHost {
                     name_index,
                     host_class_index,
                 } => AttributeInfo::NestHost {
-                    name_index: *cp_index_map.get(name_index).unwrap(),
-                    host_class_index: *cp_index_map.get(host_class_index).unwrap(),
+                    name_index: cp_index_map.get(*name_index),
+                    host_class_index: cp_index_map.get(*host_class_index),
                 },
             });
         }
@@ -292,21 +308,34 @@ impl ShuffleConstantPool {
     }
 }
 
+struct CPIndexMap {
+    /// Internal mapping of constant pool indices: uses range [[ `0` ; `cp.len()-1` ]].
+    map: HashMap<u16, u16>,
+}
+
+impl CPIndexMap {
+    /// The input index is assumed to be in the range [[ `1` ; `cp.len()` ]].
+    fn get(&self, cp_index: u16) -> u16 {
+        assert!(cp_index >= 1);
+        self.map.get(&(cp_index - 1)).unwrap() + 1
+    }
+}
+
 impl ClassFileTransformation for ShuffleConstantPool {
     fn transform(&self, cf: &ClassFile) -> ClassFile {
-        let cp_index_map: HashMap<u16, u16> = self.shuffle_indices(&cf.constant_pool);
+        let cp_index_map: CPIndexMap = self.shuffle_indices(&cf.constant_pool);
 
         ClassFile {
             minor_version: cf.minor_version,
             major_version: cf.major_version,
             constant_pool: self.modify_constant_pool(&cp_index_map, &cf.constant_pool),
             access_flags: cf.access_flags,
-            this_class: *cp_index_map.get(&cf.this_class).unwrap(),
-            super_class: *cp_index_map.get(&cf.super_class).unwrap(),
+            this_class: cp_index_map.get(cf.this_class),
+            super_class: cp_index_map.get(cf.super_class),
             interfaces: cf
                 .interfaces
                 .iter()
-                .map(|interface_index| *cp_index_map.get(interface_index).unwrap())
+                .map(|interface_index| cp_index_map.get(*interface_index))
                 .collect(),
             fields: self.modify_fields(&cp_index_map, &cf.constant_pool, &cf.fields),
             methods: self.modify_methods(&cp_index_map, &cf.constant_pool, &cf.methods),
